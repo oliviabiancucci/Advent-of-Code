9d343b481473b3d64661e256537f0694dac04b3d
Olivia Biancucci <64789589+oliviabiancucci@users.noreply.github.com>
2022-12-05 13:20:41 -0500

    Create day1_p1.c

---
+++ day1_p1.c
@@ -0,0 +1,38 @@
+//Advent of Code 2022 Day 1 Part 1 by Olivia Biancucci
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(){
+  int largestSum = 0;
+  int currNum = 0;
+  int currSum = 0;
+  char buffer[1024];
+
+  FILE * fp = fopen("input.txt", "r");
+  if (fp == NULL){
+    printf("Error opening file.\n");
+    return(-1);
+  }
+
+  fgets(buffer, sizeof(buffer), fp);
+  while(!feof(fp)){
+    if ((strcmp(buffer, "\r\n") == 0) || (strcmp(buffer, "\n") == 0)){
+      if (currSum > largestSum){
+        largestSum = currSum;
+      }
+        currSum = 0;
+    }
+    else{
+      currNum = atoi(buffer);
+      currSum += currNum;
+    }
+    fgets(buffer, sizeof(buffer), fp);
+  }
+
+  printf("Largest Sum: %d\n", largestSum);
+
+  fclose(fp);
+  return 0;
+}
